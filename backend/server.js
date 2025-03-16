fc6e27c0b7d9b9cf1c2e5513628f71edc7f5bb71
Kamal Preetam Chittuluri <preetamkamal@gmail.com>
2025-03-16 14:33:51 -0400

    fixed

--- backend/server.js
+++ backend/server.js
@@ -384,9 +384,9 @@ app.post('/api/mcqs/submit', authenticateToken, (req, res) => {
     });
   });
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-  });
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+//   });
   
 
 
